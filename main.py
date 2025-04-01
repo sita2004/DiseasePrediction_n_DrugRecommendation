3c53c1cd7580133882c2b9a79f31a65503427b9d
sita2004 <151060405+sita2004@users.noreply.github.com>
2025-04-01 21:30:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -217,5 +217,5 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
